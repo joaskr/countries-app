39885775efb80d76266f51d92fefde7c3dfffa2c
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2022-12-19 23:10:47 +0100

    Started working on 404 page

--- src/middleware/router.js
+++ src/middleware/router.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Countries from '../screens/countries/Countries';
 import CountryDetails from '../screens/countryDetails/CountryDetails';
+import NotFound from '../screens/notFound/notFound';
+
 const Routing = () => {
   return (
     <Routes>
       <Route path="/countries" element={<Countries />} />
       <Route path="/countries/:code" element={<CountryDetails />} />
       <Route path="/" element={<Countries />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
---
+++ src/screens/notFound/notFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <h1>This page does not exist</h1>
+      <button>Go back</button>
+    </div>
+  );
+}
+
+export default NotFound;
