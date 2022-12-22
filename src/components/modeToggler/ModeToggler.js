dc03459874a11513465d3e854d9273557edac44a
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2022-12-22 14:53:28 +0100

    Started working on theme toggler

--- src/components/modeToggler/ModeToggler.js
+++ src/components/modeToggler/ModeToggler.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon } from '@fortawesome/free-regular-svg-icons';
 import './ModeToggler.scss';
+import '../../index.scss';
 
 export default function ModeToggler() {
+  const [activeTheme, setActiveTheme] = useState('light');
+
+  const changeTheme = () => {
+    activeTheme === 'light' ? setActiveTheme('dark') : setActiveTheme('light');
+    document.documentElement.className = '';
+    document.documentElement.classList.add(`theme-${activeTheme}`);
+  };
+
   return (
-    <button className="toggler-btn">
+    <button className="toggler-btn" onClick={changeTheme}>
       <FontAwesomeIcon icon={faMoon} className="toggler-icon" />
       <strong>Dark Mode</strong>
     </button>
