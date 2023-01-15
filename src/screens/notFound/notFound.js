49c32806d438ef090e2042dfb0176d32c531d8e5
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2023-01-15 23:40:42 +0100

    Continued with adding theme switch

--- src/screens/notFound/notFound.js
+++ src/screens/notFound/notFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './notFound.scss';
+import Button from '../../components/button/Button';
 
 function NotFound() {
   return (
-    <div>
-      <h1>This page does not exist</h1>
-      <button>Go back</button>
+    <div className="not-found-container">
+      <h2 className="not-found-header">This page does not exist</h2>
+      <Button navButton={true} text="Back" url="/countries" />
     </div>
   );
 }
