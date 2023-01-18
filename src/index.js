0088882db6c1fefae1dc114cb321316d700779be
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2023-01-18 23:06:46 +0100

    removed unused files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import Middleware from './middleware/middleware';
 import Navbar from './components/navbar/Navbar';
@@ -22,10 +21,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-//todo - dark mode, mobile view, add error msg to both sites, create 404, pagination
