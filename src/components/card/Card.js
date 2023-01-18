54ab62546a91221ac51b1843e2d091b8ec493a42
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2023-01-18 22:42:06 +0100

    Added animation

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,22 +3,29 @@ import './Card.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import DataParagraph from '../dataParagraph/DataParagraph';
+import { motion } from 'framer-motion';
 
 function Card({ image, name, population, region, capital, cca2 }) {
   return (
-    <div className="card">
-      <div className="card-header">
-        <img src={image} alt={'flag of ' + name} />
-      </div>
-      <div className="card-content">
-        <Link to={`/countries/${cca2}`}>
+    <motion.div
+      layout
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      whileHover={{ scale: 1.1 }}
+      className="card">
+      <Link to={`/countries/${cca2}`} className="card-link">
+        <div className="card-header">
+          <img src={image} alt={'flag of ' + name} />
+        </div>
+        <div className="card-content">
           <h2>{name}</h2>
-        </Link>
-        <DataParagraph category="Population" info={population} />
-        <DataParagraph category="Region" info={region} />
-        <DataParagraph category="Capital" info={capital} />
-      </div>
-    </div>
+          <DataParagraph category="Population" info={population} />
+          <DataParagraph category="Region" info={region} />
+          <DataParagraph category="Capital" info={capital} />
+        </div>
+      </Link>
+    </motion.div>
   );
 }
 
