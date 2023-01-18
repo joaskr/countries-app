39e8d1f38f395570239717244374d2e85564dbda
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2023-01-18 22:07:37 +0100

    Fixed some responsiveness issues

--- src/screens/countries/Countries.js
+++ src/screens/countries/Countries.js
@@ -6,9 +6,12 @@ import Spinner from '../../components/spinner/Spinner';
 import { getCountries, getCountriesbyName, getCountryByRegion } from '../../middleware/index';
 import Input from '../../components/input/Input';
 import SelectInput from '../../components/selectInput/SelectInput';
+// import { motion, AnimatePresence } from 'framer-motion';
 
 function Countries() {
   const [countries, setCountries] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
@@ -16,12 +19,16 @@ function Countries() {
   }, []);
 
   const getAllCountries = () => {
+    setError(null);
+    setLoading(true);
     getCountries()
       .then((res) => {
         setCountries(res.data);
+        setLoading(false);
       })
       .catch((err) => {
-        console.log(err);
+        setError(err);
+        setLoading(false);
       });
   };
 
@@ -29,19 +36,13 @@ function Countries() {
     if (e.target.value === '') {
       getAllCountries();
     } else {
+      setError(null);
       getCountriesbyName(e.target.value)
         .then((res) => {
           setCountries(res.data);
         })
-        .catch(() => {
-          setCountries([
-            {
-              flags: {
-                png: 'https://images.unsplash.com/photo-1526778548025-fa2f459cd5c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80'
-              },
-              name: { common: 'Country Not found' }
-            }
-          ]);
+        .catch((err) => {
+          setError(err);
         });
     }
   };
@@ -51,15 +52,41 @@ function Countries() {
     if (e.target.value === '') {
       getAllCountries();
     } else {
+      setError(null);
       getCountryByRegion(e.target.value)
         .then((res) => {
           setCountries(res.data);
         })
         .catch((err) => {
-          console.log(err);
+          setError(err);
         });
     }
   };
+
+  if (loading) {
+    return (
+      <div className="spinner-div">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="countries-wrapper">
+        <header className="countries-header">
+          <Input onChangeFunc={getFilteredCountries} searchTerm={searchTerm} />
+          <SelectInput onChangeFunc={selectEventHandler} />
+        </header>
+        <main>
+          <div className="not-found-container">
+            <h2 className="not-found-header">Something went wrong.</h2>
+            <p>{error.message}</p>
+          </div>
+        </main>
+      </div>
+    );
+  }
 
   return (
     <div className="countries-wrapper">
@@ -68,23 +95,21 @@ function Countries() {
         <SelectInput onChangeFunc={selectEventHandler} />
       </header>
       <main className="countries-content">
-        {countries === false ? (
-          <Spinner />
-        ) : (
-          countries.map((country, id) => {
-            return (
-              <Card
-                key={id}
-                image={country.flags.png}
-                name={country.name.common}
-                population={country.population}
-                region={country.region}
-                capital={country.capital}
-                cca2={country.cca2}
-              />
-            );
-          })
-        )}
+        {countries === false
+          ? null
+          : countries.map((country, id) => {
+              return (
+                <Card
+                  key={id}
+                  image={country.flags.png}
+                  name={country.name.common}
+                  population={country.population}
+                  region={country.region}
+                  capital={country.capital}
+                  cca2={country.cca2}
+                />
+              );
+            })}
       </main>
     </div>
   );
